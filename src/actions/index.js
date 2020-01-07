0bdf55953391f9b83b9c42bc495bff03dedfb576
Oskhar Arrieta <oskhara@uninorte.edu.co>
2020-01-07 09:41:16 -0500

    Project Finished

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,17 @@
+import _ from 'lodash';
 import jsonPlaceholder from '../apis/jsonPlaceholder';
+
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+    await dispatch(fetchPosts());
+    // const userIds=_.uniq(_.map(getState().posts, 'userId'));
+    // userIds.forEach(id => dispatch(fetchUser(id)));
+
+    _.chain(getState().posts)
+        .map('userId')
+        .uniq()
+        .forEach(id => dispatch(fetchUser(id)))
+        .value();
+};
 
 export const fetchPosts = () => async dispatch => {
     const response = await jsonPlaceholder.get('/posts');
@@ -17,3 +30,14 @@ export const fetchUser = id => async dispatch => {
     })
 
 };
+
+// export const fetchUser = id => async dispatch => {
+//     _fetchUser(id,dispatch);
+// };
+// const _fetchUser = _.memoize(async(id,dispatch)=>{
+//     const response = await jsonPlaceholder.get(`/users/${id}`);
+//     dispatch({
+//         type: 'FETCH_USER',
+//         payload: response.data
+//     })
+// });
--- src/components/UserHeader.js
+++ src/components/UserHeader.js
@@ -3,9 +3,7 @@ import { connect } from 'react-redux';
 import { fetchUser } from '../actions';
 
 class UserHeader extends React.Component{
-    componentDidMount(){
-        this.props.fetchUser(this.props.userId);
-    }
+    
     render(){
         const { user } = this.props;
 
